3eeecf412d63a90f04c6bb846424672329b0a3b7
Newton Neto <newton.mgs@gmail.com>
2022-08-02 15:41:50 -0300

    map for firebase auth exceptions

---
+++ src/maps/firebaseExceptions.ts
@@ -0,0 +1,7 @@
+const firebaseExceptions: { [key: string]: string } = {
+  'auth/invalid-email': 'E-mail inválido',
+  'auth/wrong-password': 'E-mail ou senha inválida',
+  'auth/user-not-found': 'E-mail ou senha inválida',
+};
+
+export default firebaseExceptions;
