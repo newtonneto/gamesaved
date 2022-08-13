ee7e62b7f8393d979e0006461caf7f5bb04821f2
Newton Neto <newton.mgs@gmail.com>
2022-08-13 17:30:58 -0300

    file name update

--- src/modules/HomeStack/index.tsx
+++ src/modules/HomeStack/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from '../../screens/Home';
-import GameDetails from '../../screens/Game';
+import GameDetails from '../../screens/GameDetails';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
