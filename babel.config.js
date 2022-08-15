5dad1307b2a09902f59d5687226ed565c22c173a
Newton Neto <newton.mgs@gmail.com>
2022-08-15 15:10:09 -0300

    using enviroment file

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: true,
+        allowUndefined: false,
+        verbose: false,
+      },
+    ],
+  ],
 };
