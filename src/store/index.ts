8dfd6af79a8daed6ee66c2fb8880e0de27b5a746
Newton Neto <newton.mgs@gmail.com>
2022-09-02 16:59:04 -0300

    feat: remove unused slice
    Ref: #5

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,10 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 
 import navigationReducer from '@store/slices/navigation-slice';
-import userReducer from '@store/slices/user-slice';
 
 const store = configureStore({
   reducer: {
     navigationReducer: navigationReducer,
-    userReducer: userReducer,
   },
 });
 
