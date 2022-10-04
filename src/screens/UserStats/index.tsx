abe6885d6fbc1d8fb8ed57ea59d030d7e8f144d9
Newton Neto <newton.mgs@gmail.com>
2022-10-04 18:23:44 -0300

    feat: invite/ban fab button
    Ref: #38

--- src/screens/UserStats/index.tsx
+++ src/screens/UserStats/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Alert } from 'react-native';
-import { Avatar, useToast } from 'native-base';
+import { Avatar, Fab, useTheme, useToast } from 'native-base';
 import storage from '@react-native-firebase/storage';
 import firestore, {
   FirebaseFirestoreTypes,
@@ -10,6 +10,8 @@ import {
   useIsFocused,
   useNavigation,
 } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { UserCirclePlus, UserCircleMinus } from 'phosphor-react-native';
 
 import Toast from '@components/Toast';
 import VStack from '@components/VStack';
@@ -22,6 +24,7 @@ import { useAppDispatch } from '@store/index';
 import { setTitle } from '@store/slices/navigation-slice';
 import { AXIS_X_PADDING_CONTENT, TOAST_DURATION } from '@utils/constants';
 import firestoreValueIsValid from '@utils/firestoreValueIsValid';
+import { PartyDto } from '@src/interfaces/party.dto';
 
 type RouteParams = {
   uuid: string;
@@ -29,17 +32,23 @@ type RouteParams = {
 
 const UserStats = () => {
   const toast = useToast();
+  const { colors } = useTheme();
   const navigation = useNavigation();
   const dispatch = useAppDispatch();
   const route = useRoute();
   const isFocused = useIsFocused();
   const { uuid } = route.params as RouteParams;
+  const [isMember, setIsMember] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [profile, setProfile] = useState<ProfileDto>({} as ProfileDto);
   const [image, setImage] = useState<string | undefined>(undefined);
+  const userSession: FirebaseAuthTypes.User = auth().currentUser!;
   const profileRef = useRef<
     FirebaseFirestoreTypes.DocumentReference<ProfileDto>
   >(firestore().collection<ProfileDto>('profiles').doc(uuid));
+  const partyRef = useRef<FirebaseFirestoreTypes.DocumentReference<PartyDto>>(
+    firestore().collection<PartyDto>('parties').doc(userSession.uid),
+  );
 
   useEffect(() => {
     let isMounted = true;
@@ -50,6 +59,27 @@ const UserStats = () => {
       isMounted = false;
     };
   }, [isFocused]);
+
+  const checkMember = async () => {
+    try {
+      const response = (await partyRef.current.get()).data();
+
+      if (response !== undefined) {
+        setIsMember(response.members.includes(uuid));
+      }
+    } catch {
+      Alert.alert('>.<', 'Conteúdo indisponível, tente novamente mais tarde.', [
+        {
+          text: 'Voltar',
+          onPress: () => navigation.goBack(),
+        },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    checkMember();
+  }, []);
 
   const getImage = async (avatarRef: string) => {
     if (firestoreValueIsValid(avatarRef)) {
@@ -108,73 +138,91 @@ const UserStats = () => {
   return (
     <VStack px={AXIS_X_PADDING_CONTENT}>
       {!isLoading ? (
-        <ScrollView pt={8}>
-          <Avatar
-            bg="gray.700"
-            alignSelf="center"
-            size="2xl"
-            mb={8}
-            source={{
-              uri: image,
-            }}>{`${profile.firstName[0]}${profile.lastName[0]}`}</Avatar>
+        <>
+          <Fab
+            placement="bottom-right"
+            renderInPortal={false}
+            shadow={2}
+            bg={isMember ? 'gray.700' : 'secondary.700'}
+            icon={
+              isMember ? (
+                <UserCircleMinus color={colors.white} size={18} />
+              ) : (
+                <UserCirclePlus color={colors.white} size={18} />
+              )
+            }
+            label={isMember ? 'Ban Member' : 'Invite Member'}
+            onPress={() => {}}
+            _pressed={{ bg: 'gray.500' }}
+          />
+          <ScrollView pt={8}>
+            <Avatar
+              bg="gray.700"
+              alignSelf="center"
+              size="2xl"
+              mb={8}
+              source={{
+                uri: image,
+              }}>{`${profile.firstName[0]}${profile.lastName[0]}`}</Avatar>
 
-          <Attribute
-            type="sword"
-            value={profile.username}
-            svg={false}
-            label="Username"
-          />
-          <Attribute
-            type="shield"
-            value={`${profile.firstName} ${profile.lastName}`}
-            svg={false}
-            label="Nome"
-          />
-          <Attribute
-            type="magicWand"
-            value={profile.email}
-            svg={false}
-            label="Email"
-          />
-          <Attribute
-            type="genderIntersex"
-            value={genderTranslator[profile.gender]}
-            svg={false}
-            label="Gênero"
-          />
-          {firestoreValueIsValid(profile.psnId) && (
             <Attribute
-              type="playstation"
-              value={profile.psnId}
-              svg={true}
-              label="PSN ID"
+              type="sword"
+              value={profile.username}
+              svg={false}
+              label="Username"
             />
-          )}
-          {firestoreValueIsValid(profile.xboxGamertag) && (
             <Attribute
-              type="xbox"
-              value={profile.psnId}
-              svg={true}
-              label="Xbox Gamertag"
+              type="shield"
+              value={`${profile.firstName} ${profile.lastName}`}
+              svg={false}
+              label="Nome"
             />
-          )}
-          {firestoreValueIsValid(profile.nintendoAccount) && (
             <Attribute
-              type="nintendo"
-              value={profile.psnId}
-              svg={true}
-              label="Nintendo Account"
+              type="magicWand"
+              value={profile.email}
+              svg={false}
+              label="Email"
             />
-          )}
-          {firestoreValueIsValid(profile.steamProfile) && (
             <Attribute
-              type="steam"
-              value={profile.psnId}
-              svg={true}
-              label="Steam Profile"
+              type="genderIntersex"
+              value={genderTranslator[profile.gender]}
+              svg={false}
+              label="Gênero"
             />
-          )}
-        </ScrollView>
+            {firestoreValueIsValid(profile.psnId) && (
+              <Attribute
+                type="playstation"
+                value={profile.psnId}
+                svg={true}
+                label="PSN ID"
+              />
+            )}
+            {firestoreValueIsValid(profile.xboxGamertag) && (
+              <Attribute
+                type="xbox"
+                value={profile.psnId}
+                svg={true}
+                label="Xbox Gamertag"
+              />
+            )}
+            {firestoreValueIsValid(profile.nintendoAccount) && (
+              <Attribute
+                type="nintendo"
+                value={profile.psnId}
+                svg={true}
+                label="Nintendo Account"
+              />
+            )}
+            {firestoreValueIsValid(profile.steamProfile) && (
+              <Attribute
+                type="steam"
+                value={profile.psnId}
+                svg={true}
+                label="Steam Profile"
+              />
+            )}
+          </ScrollView>
+        </>
       ) : (
         <Loading />
       )}
