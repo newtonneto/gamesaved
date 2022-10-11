468188b15c989351e8cfd22099568349c2923fee
Newton Neto <newton.mgs@gmail.com>
2022-10-10 22:54:53 -0300

    feat: guild screen base code
    Ref: #27

--- src/screens/Guild/index.tsx
+++ src/screens/Guild/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import VStack from '@components/VStack';
+import Loading from '@components/Loading';
+import FlatList from '@components/FlatList';
+import { AXIS_X_PADDING_CONTENT } from '@utils/constants';
 
 const Guild = () => {
-  return <></>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  return (
+    <VStack px={AXIS_X_PADDING_CONTENT}>
+      {!isLoading ? (
+        <FlatList
+          data={[]}
+          renderItem={() => null}
+          showsVerticalScrollIndicator={false}
+        />
+      ) : (
+        <Loading />
+      )}
+    </VStack>
+  );
 };
 
 export default Guild;
